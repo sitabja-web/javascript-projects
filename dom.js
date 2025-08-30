4836e325bac9129fe9ed86232f7c83a62c7891e2
sitabja biswas <sitabjabiswas633@gmail.com>
2025-08-31 02:15:58 +0530

    Add files via upload

---
+++ dom.js
@@ -0,0 +1,54 @@
+console.log("ayush")
+
+console.assert(444>555) // 
+
+
+console.clear()     // this clears all the things 
+
+// parse int convers a string to a number 
+
+console.log(document.title)  // this gives you the html element 
+
+
+// document.title = "ayush" 
+
+
+// document.body.style.backgroundColor  = "red"  //  this changes the color to red
+
+
+// document.body.style.fontSize  = "50px" 
+
+
+document.body.firstChild()  = > // this gives the  text area if there is some text area otherewise it will show the navar 
+
+document.body.firstElementChild()  // this  gives the tag with the  with out the text areaa
+
+document.body.childNodes()  //  this gives all the tags that we have there in the tags
+
+document.body.parentElement()   // this gives the parent element which is html
+
+// by same process we can calculate the siblings 
+// next element siblnig and like that 
+
+let a = document.getElementById("about")
+a.style.color = "red"     // this change the  color 
+
+
+let b  = document.getElementsByClassName("navbar")
+b[0].style.backgroundcolr = "blue "  //  this change the color 
+
+let a = document.querySelectorAll(".nav_bar")
+a[0].style.color =  "red"
+
+console.log(document.getElementsByName("search"))  
+// id preference is more than class 
+
+
+let a = document.getElementById("home")
+console.log(a.matches = ".item")   // this is done to check 
+
+
+
+// b.closest => check for the nearest element selector 
+// b.contains  check if it contains the same thihng or not 
+
