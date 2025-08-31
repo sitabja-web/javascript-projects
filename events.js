7088ed7a95f293849425f4a46cd32dbf2ac252ad
sitabja biswas <sitabjabiswas633@gmail.com>
2025-08-31 12:42:06 +0530

    Add files via upload

---
+++ events.js
@@ -0,0 +1,30 @@
+// here we have id  \\
+
+let a = document.getElementById("box")
+
+a.hasAttribute("box")
+  // true output 
+
+// we also have set attribute 
+
+// box.insertAdjacentHTML(
+// )
+
+
+// class name 
+
+box.classList("ayush")
+box.classList.remvoe("ayush")
+box.classList.replace("ayush")
+box.classList.toggle("ayush")      // it adds then again it becomees false
+
+
+
+let a = document.getElementById("box")
+setTimeout(function() {
+    alert("vrtual code ")
+ },3000)
+
+clearTimeout()   //  it  removes the set timeout 
+
+// we use set interval if we want to if we want to call some one after some interval repeatedly 
