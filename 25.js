9e52f417137b470cd88e73f205a2b13a2d94dccd
sitabja biswas <sitabjabiswas633@gmail.com>
2025-09-05 13:36:13 +0530

    Add files via upload

---
+++ 25.js
@@ -0,0 +1,123 @@
+// in case of js we prefer  not to use var because it has a function scope not a var scope 
+// it is because of these in js we face many issue because of hoisting and let const
+
+
+// we use strick mode to  all js code as newer version
+
+// data types in js 
+
+ // undefined (because no value is assgined)
+
+
+// object =>   arrays , function , objects
+
+
+// console.log( null == undefined)
+
+
+// const balance = new Number(100)
+// console.log(balance);
+
+// console.log(balance.toString().length)
+
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+// const score = 100
+// const scoreValue = 100.3
+
+// const isLoggedIn = false
+// const outsideTemp = null
+// let userEmail;
+
+// const id = String('123')
+// const anotherId = String('123')
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+// const heros = ["shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
+// }
+
+// const myFunction = function(){
+//     console.log("Hello world");
+// }
+
+// console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+
+
+// Dates
+
+let myDate = new Date()
+console.log(myDate.toString());
+console.log(myDate.toDateString());
+console.log(myDate.toLocaleString());
+console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023, 0, 23)
+// let myCreatedDate = new Date(2023, 0, 23, 5, 3)
+// let myCreatedDate = new Date("2023-01-14")
+// let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+// let myTimeStamp = Date.now()
+
+// console.log(myTimeStamp);
+// console.log(myCreatedDate.getTime());
+// console.log(Math.floor(Date.now()/1000));
+
+// let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth() + 1);
+// console.log(newDate.getDay());
+
+// `${newDate.getDay()} and the time `
+
+// newDate.toLocaleString('default', {
+//     weekday: "long",
+    
+// })
+
+
+
+// day 2
+
+// arrays in js store the same type of variable
+
+// const arr = [27 , "ayush " , true , "dev"]
+
+// console.log(arr.length)
+// console.log(arr[2])
+
+// we can change the arrays but not string after declaring 
+// type of array is object 
+
+// slice and splice  can be same by the sound but their functionsliy is different 
+
+
+// array
+
+
+// Array methods
+
+//  objects is a key - value pair 
+
+
+
+// slice, splice
+
